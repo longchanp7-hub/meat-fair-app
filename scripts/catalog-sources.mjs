b9c0b542c71c1d582be143927a653e25bca8cfe8
longchanp7-hub <longchanp7@gmail.com>
2026-09-18 21:20:10 +0900

    fix: add current student and Roan catalog sources

--- scripts/catalog-sources.mjs
+++ scripts/catalog-sources.mjs
@@ -1,9 +1,11 @@
 // Stable fallback entry points observed in official pages. Discovery still
 // follows actual links; these are not constructed image URLs or price records.
 export const CATALOG_SOURCES={
+  'jukusei-ichiban':['https://www.jukusei-ichiban.jp/jp/menu/'],
+  'syabuyo':['https://www.skylark.co.jp/syabuyo/gakusei/index.html'],
   'washoku-sato':['https://sato-res.com/sato/en/menu/','https://sato-res.com/sato/bar/','https://sato-res.com/satoshabu/','https://sato-res.com/satosuki/','https://sato-res.com/satoyaki/'],
   'kalubi-taisho':['https://www.kalubi-taisho.com/menu/3616/','https://www.kalubi-taisho.com/menu/4234/'],
-  'roan':['https://www.good-promise.co.jp/roan/price/','https://www.good-promise.co.jp/roan/shop/toyokawa/','https://www.good-promise.co.jp/roan/shop/hamamatsu/'],
+  'roan':['https://ameblo.jp/0141roan/entry-12977830217.html','https://www.good-promise.co.jp/roan/price/','https://www.good-promise.co.jp/roan/shop/toyokawa/','https://www.good-promise.co.jp/roan/shop/hamamatsu/'],
   'stamina-taro':['https://staminataro.jp/shop-list/'],
   'asakuma':['https://www.asakuma.co.jp/shoplist/okazaki.html','https://www.asakuma.co.jp/shoplist/hamamatsuic.html'],
   'anrakutei':['https://anrakutei.jp/topic/20260917pricedown/']
